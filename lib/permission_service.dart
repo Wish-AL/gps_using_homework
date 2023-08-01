760067deef1cd8c4b52b31813f9e72b46a5dc92c
Oleksandr Vyshniakov <ihtis.wa@gmail.com>
2023-08-01 16:43:23 +0300

    Initial get location data stream by geolocator plugin

---
+++ lib/permission_service.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LocationService extends ChangeNotifier {
+  Position? position;
+  LocationPermission? permission;
+  bool serviceEnabled = false;
+  bool isGranted = false;
+  StreamSubscription<Position>? positionStream;
+  double? latitude;
+  double? longitude;
+
+  Future<bool> isLocationServiceEnabled() async {
+    return await Geolocator.isLocationServiceEnabled();
+  }
+
+  void locationPermission() async {
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+  }
+
+  void requestPermissionIfDeniedForever() {
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+    }
+  }
+
+  void getOnceTimeLocation() async {
+    position = await Geolocator.getCurrentPosition();
+    notifyListeners();
+  }
+
+  Future<void> currentLocation() async {
+    if( await isLocationServiceEnabled()) {
+      serviceEnabled = true;
+      positionStream =
+          Geolocator.getPositionStream().listen((Position? position) {
+            latitude = position?.latitude;
+            longitude = position?.longitude;
+            notifyListeners();
+          });
+    }
+  }
+  void stop() {
+    positionStream?.cancel();
+  }
+}
