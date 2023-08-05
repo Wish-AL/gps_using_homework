fc68eb46d4685a10192419399102b7ca0bbec8c1
Oleksandr Vyshniakov <ihtis.wa@gmail.com>
2023-08-06 01:31:53 +0300

    create cubit

--- lib/application/gps_location_app.dart
+++ lib/application/gps_location_app.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:gps_homework/location_cubit.dart';
+//import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../permission_service.dart';
+//import '../permission_service.dart';
 
 class GpsLocationApp extends StatelessWidget {
   const GpsLocationApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => LocationService(),
+    return BlocProvider(
+      create: (BuildContext context) => LocationCubit(),
       child: MaterialApp(
         title: 'My location',
         theme: ThemeData(
@@ -33,7 +35,7 @@ class LocationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final location = context.read<LocationService>();
+    final location = context.read<LocationCubit>();
     return Column(
       children: [
         const LocationDataText(),
@@ -65,8 +67,11 @@ class LocationDataText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final locationW = context.watch<LocationService>();
-    return Text(
-        'Latitude: ${locationW.latitude ?? 0}, Longitude: ${locationW.longitude ?? 0}');
+    //final locationW = context.watch<LocationCubit>();
+    return BlocBuilder<LocationCubit, LocationState>(builder: (context, state){
+      return Text(
+          'Latitude: ${state.latitude ?? 0}, Longitude: ${state.longitude ?? 0}');
+    });
+
   }
 }
